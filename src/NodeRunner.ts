4fd017244fd7228f3ae7defa214a7c8db4cebe9a
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-12-30 15:26:59 +0530

    Added name

--- src/NodeRunner.ts
+++ src/NodeRunner.ts
@@ -19,7 +19,7 @@ r.resolve = (oldRequire as any).resolve;
 Module.prototype.require = r;
 const oldCompile = Module.prototype._compile;
 Module.prototype._compile = function (content, filename, format) {
-    content = content.replace(/System\.register\s{0,20}\(/, `System.register("${filename}",`);
+    content = content.replace(/System\.register\s{0,20}\(/, `System.register(${ JSON.stringify(filename)},`);
     return oldCompile.call(this, content, filename, format);
 }
 declare var global;
